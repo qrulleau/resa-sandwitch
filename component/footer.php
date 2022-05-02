d22fc86d72310bd869f6bc2e7921c84a12548ea5
qrulleau <quentin.rulleau@yahoo.com>
2022-05-02 13:29:18 +0200

    ADD: style on footer & header component

---
+++ component/footer.php
@@ -0,0 +1,31 @@
+<footer>
+  <div class="container">
+    <div class="d-flex align-start">
+      <div>
+        <h5 class="title">Reservation sandwitch Saint-Vincent</h5>
+        <p>30 rue de Meaux</p>
+        <p>60300 Senlis</p>
+      </div>
+      <div>
+        <h5 class="title">Nous Contacter</h5>
+        <p>Tel: 03.44.53.96.40</p>
+        <p>Fax: 03.44.53.58.96</p>
+        <a href="https://www.lycee-stvincent.fr/">Web : https://www.lycee-stvincent.fr/</a>
+      </div>
+      <div>
+        <h5 class="title">Acces rapide</h5>
+        <div class="d-flex column align-start">
+          <a href="">Accueil</a>
+          <a href="">Vos commandes</a>
+        </div>
+      </div>
+      <div>
+        <h5 class="title">Suivez-nous</h5>
+        <div class="d-flex social-link justify-center">
+          <a href=""><img src="../assets/icon/icon-facebook.svg" alt=""></a>
+          <a href=""><img src="../assets/icon/icon-twitter.svg" alt=""></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
--- component/header.php
+++ component/header.php
@@ -1,7 +1,11 @@
 <header>
-  <div class="d-flex">
+  <div class="d-flex container">
     <div class="d-flex">
-      <img src="../assets/logo.png" alt="">
+      <div class="logo t-center">
+        <img src="../assets/logo.png" alt="">
+        <h1 class="playfair">Lycée Saint-Vincent</h1>
+        <p>Enseignement secondaire & supérieur</p>
+      </div>
       <a href="../index.php">Accueil</a>
     </div>
     <a href="../views/login.php">Se connecter</a>
