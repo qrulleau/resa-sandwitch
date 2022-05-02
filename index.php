005eba5314d15b2675a9ad0469be9720ca9a10ca
qrulleau <quentin.rulleau@yahoo.com>
2022-05-02 08:58:58 +0200

    ADD: presentation of the index page

--- index.php
+++ index.php
@@ -6,16 +6,27 @@ require_once 'database/connexion.php';
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="style/main.css">
-</head>
-<body>
-  
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/main.css">
+  </head>
+  <body>
+    <?php require 'component/header.php'?>
+    <div class="t-center">
+      <h2>Bienvenue</h2>
+      <h3>Presentation du site :</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, aut. Commodi tempora voluptas fugit libero ipsam placeat incidunt excepturi modi maiores laudantium reiciendis unde iusto asperiores harum exercitationem quisquam, minima molestias atque eos ea consectetur hic dicta? Adipisci labore, itaque nobis dicta tempore exercitationem. Itaque corrupti similique harum dignissimos! Facilis maiores nihil sapiente, sequi ab nisi itaque corrupti. Voluptatibus quae iure modi labore fuga quia eaque ipsum earum quo mollitia, ut in alias beatae quisquam cupiditate rerum ex ad eligendi id magni suscipit tempora molestias aliquam. Aspernatur expedita doloribus, ratione assumenda molestias nihil aliquid possimus doloremque. Optio reprehenderit, culpa nesciunt id libero aut molestiae architecto possimus voluptatum nihil facilis iure veniam consequatur ratione ducimus aspernatur nostrum. Rerum in porro totam sequi voluptatem! Cupiditate blanditiis ipsa porro obcaecati fugit nihil cumque aperiam? Beatae quis qui debitis, molestiae distinctio eaque excepturi illum odit quisquam perspiciatis dicta quaerat, ab architecto molestias ullam odio.</p>
+      <h3>Voici le menu de la semaine</h3>
+      <a href="assets/menu.pdf">
+        <img src="assets/menu.png" alt="">
+      </a>
+    </div>
+    
+    <?php require 'component/footer.php'?>
 
-  <script src="script/main.js"></script>
-</body>
+    <script src="script/main.js"></script>
+  </body>
 </html>
