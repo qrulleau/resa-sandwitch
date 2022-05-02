2f6f3180475fbc79df67c5cf6ba1367baaab41c8
qrulleau <quentin.rulleau@yahoo.com>
2022-05-02 08:56:27 +0200

    ADD: header & footer component

---
+++ component/header.php
@@ -0,0 +1,9 @@
+<header>
+  <div class="d-flex">
+    <div class="d-flex">
+      <img src="../assets/logo.png" alt="">
+      <a href="../index.php">Accueil</a>
+    </div>
+    <a href="../views/login.php">Se connecter</a>
+  </div>
+</header>
