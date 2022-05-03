b37fe2ab34c58f632abf67e033e09c0ef624cfea
qrulleau <quentin.rulleau@yahoo.com>
2022-05-03 11:14:20 +0200

    ADD: connexion to database

---
+++ database/connexion.php
@@ -0,0 +1,10 @@
+<?php
+
+  require '../env.php';
+
+try {
+  global $databaseConnexion; 
+  $databaseConnexion = new PDO("mysql:host={$server};dbname={$database}",$user,$pass);
+} catch (PDOException $e) {
+  echo 'There is an errror' . $e->getMessage();
+}
