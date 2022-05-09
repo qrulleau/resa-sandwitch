74b1d9a3642980261e6e07be60a71a6d1be61633
qrulleau <quentin.rulleau@yahoo.com>
2022-05-09 14:15:44 +0200

    ADD: header component for backoffice

---
+++ component/header-backoffice.php
@@ -0,0 +1,13 @@
+<header>
+  <div class="d-flex container">
+    <div class="d-flex">
+      <div class="logo t-center">
+        <img src="../../assets/logo.png" alt="">
+        <h1 class="playfair">Lycée Saint-Vincent</h1>
+        <p>Enseignement secondaire & supérieur</p>
+      </div>
+      <a href="../index.php">Accueil</a>
+    </div>
+    <a href="../../database/processing/deconnexion.php">Se deconnecter</a>
+  </div>
+</header>
