b871584ee26cc2776dd8ef82b790b93a81df495c
qrulleau <quentin.rulleau@yahoo.com>
2022-05-15 15:34:57 +0200

    ADD: dashboard for user

---
+++ views/backoffice/user/backoffice.php
@@ -0,0 +1,96 @@
+<?php 
+
+require (__DIR__ . '../../../../database/connexion.php');
+require (__DIR__ . '../../../../database/querie/querie.php');
+
+displayUtilisateurPage();
+
+var_dump($utilisateurs);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Playfair+Display:ital@1&display=swap" rel="stylesheet"> 
+  <title>Document</title>
+  <link rel="stylesheet" href="../../../style/main.css">
+</head>
+<body class="backoffice_user">
+  <div class="d-flex align-start">
+    <?php require (__DIR__ . '../../../../component/header-backoffice.php'); ?>
+
+    <div class="container">
+      <div class="header-tab white">
+        <div class="d-flex t-center">
+          <div class="col id_accueil">
+            <p>id_user</p>
+          </div>
+          <div class="col texte_accueil">
+            <p>role_user</p>
+          </div>
+          <div class="col lien_pdf">
+            <p>email_user</p>
+          </div>
+          <div class="col action">
+            <p>prenom_user</p>
+          </div>
+          <div class="col action">
+            <p>nom_user</p>
+          </div>
+          <div class="col action">
+            <p>password_user</p>
+          </div>
+          <div class="col action">
+            <p>active_user</p>
+          </div>
+        </div>
+      </div>
+      <div class="body-tab t-center">
+        
+        <?php 
+          foreach ($utilisateurs as $utilisateur) {
+        ?>
+        <div class="d-flex border">
+          <div class="col id_accueil">
+            <p><?php echo $utilisateur['id_user'] ?></p>
+          </div>
+          <div class="col texte_accueil">
+            <p><?php echo $utilisateur['role_user'] ?></p>
+          </div>
+          <div class="col lien_pdf">
+            <p><?php echo $utilisateur['email_user'] ?></p>
+          </div>
+          <div class="col lien_pdf">
+            <p><?php echo $utilisateur['prenom_user'] ?></p>
+          </div>
+          <div class="col lien_pdf">
+            <p><?php echo $utilisateur['nom_user'] ?></p>
+          </div>
+          <div class="col lien_pdf">
+            <p><?php echo $utilisateur['password_user'] ?></p>
+          </div>
+          <div class="col action">
+            <div class="d-flex justify-center">
+            <?php
+            echo "<a class='remove' href='../../../database/processing/admin-backoffice/update.php?id=" . $user['id_accueil'] . "'>Modifier</a>"
+            ?>
+            <?php
+            echo "<a class='remove' href='../../../database/processing/admin-backoffice/deleting.php?id=" . $user['id_accueil'] . "'>Supprimer</a>"
+            ?>
+            </div>
+          </div>
+        </div>
+        <?php
+          }
+        ?>
+        </div>
+      </div>
+    </div>
+  <script src="../../script/main.js"></script>
+</body>
+</html>
