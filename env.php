07a5cb51e7573aa483e9add521a3b7da7f94403c
qrulleau <quentin.rulleau@yahoo.com>
2022-05-15 15:40:43 +0200

    ADD: identifier

--- env.php
+++ env.php
@@ -1,15 +1,6 @@
-<?php
-	// Pour s'identifier, nous allons choisir notre nom d'utilisateur et mot de passe
+<?php 
 
-    // Variable contenant l'adresse du serveur web
-	$server = "localhost";
-
-    // Variable contenant le login de l'utilisateur qui a les droits sur la base de données, ça permet de définir le nom d'utilisateur
-	$user = "root";
-
-    // Variable contenant le mot de passe correspondant au login de l'utilisatur qui a les droits sur la base de données, permet de défnir le mdp
-	$pass = "";
-
-    // Nom de la base de données
-	$database = "reservesandwich";
-?>
+$user = 'qrulleau';
+$pass = 'root';
+$server = 'localhost';
+$database = 'reservesandwich';
