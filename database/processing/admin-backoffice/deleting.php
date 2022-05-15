9ad8b1ca3115f677dd3ed8099549409eae21ed1e
qrulleau <quentin.rulleau@yahoo.com>
2022-05-15 15:36:35 +0200

    MIN: change path of file

---
+++ database/processing/admin-backoffice/deleting.php
@@ -0,0 +1,11 @@
+<?php
+
+require '../../connexion.php';
+global $databaseConnexion;
+$id = $_GET['id'];
+$querie = 'DELETE FROM accueil where id_accueil = ?';
+$statement = $databaseConnexion->prepare($querie);
+$statement->execute(array($id));
+var_dump($id);
+header("location: ../../../views/backoffice/admin-backoffice/backoffice.php");
+
