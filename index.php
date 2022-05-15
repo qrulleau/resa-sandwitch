bbd69fe6b9ebaffa91f9cb164d68b2797459e6d2
qrulleau <quentin.rulleau@yahoo.com>
2022-05-15 18:07:17 +0200

    cette connase de carla a tout niquer

--- index.php
+++ index.php
@@ -14,14 +14,10 @@ displayHomePage();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Playfair+Display:ital@1&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
     <title>Document</title>
     <link rel="stylesheet" href="style/main.css">
       <!-- CSS only -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"">
-      <!-- JavaScript Bundle with Popper -->
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
   </head>
   <body>
     <?php require 'component/header.php'?>
@@ -44,7 +40,7 @@ displayHomePage();
           </div>
           <div>
             <img src="assets/accueil.jpg" class="shadow" alt="">
-            <?php echo "<img src='assets/{$projects[0]["lien_pdf"]}' alt=''>" ?>;
+            <?php echo "<img src='assets/{$projects[0]["lien_pdf"]}' alt=''>" ?>
 
           </div>
         </div>
