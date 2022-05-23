2dad217ff3f04bd48216459a86b0b0bb0ce0b4d0
Rayfy07 <thibautbourdon77@gmail.com>
2022-05-23 16:09:20 +0200

    ADD : Correction formulaire

--- views/backoffice/user/reservIndivi.php
+++ views/backoffice/user/reservIndivi.php
@@ -126,7 +126,7 @@
             /** Saisie des données du formulaire **/
             $saisieProjet = $databaseConnexion->prepare("INSERT INTO commande(fk_user_id, fk_sandwich_id, fk_boisson_id, fk_dessert_id, chips_com, date_heure_com, date_heure_livraison_com, annule_com) VALUES(?,?,?,?,?,?,?,?);");
             $saisieProjet->execute(array($id, $sandwichForm, $boissonForm, $dessertForm, $chipsForm, $dateTimeNow, $dateTimeForm, $annule_com));
-            header("Location: reservIndivi.php");
+            header("Location: reservIndivi.php?id=$id");
             $sendForm = "Votre réservation a bien été enregistrée !";
         }
     }
@@ -263,7 +263,7 @@
             </div>
 
             <!-- Formulaire pour la commande individuelle -->
-            <form id="formSaisieInfo" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>" role="form">
+            <form id="formSaisieInfo" method="POST" role="form">
                 <h3>Choisissez votre repas :</h3>
                 <div class="saisieInfo">
                     <!-- Div de liste déroulante pour les sanwichs et les desserts -->
